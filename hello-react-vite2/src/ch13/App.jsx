66dd21e23a695bb5cd16bbcaedbaf0ae9479dced
lsy3709 <lsy3709@naver.com>
2026-05-11 10:12:00 +0900

    2교시, URL 파리미터 이용한 예시

--- hello-react-vite2/src/ch13/App.jsx
+++ hello-react-vite2/src/ch13/App.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Link, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
+import Ex1 from './ex/Ex1';
+import Profile from './pages/Profile';
 
 const App = () => {
   return (
@@ -10,13 +12,16 @@ const App = () => {
       <h2>"react-router-dom": "^7.15.0"</h2>
       {/* 내비게이션 메뉴 */}
       <nav>
-        <Link to="/">홈</Link> | <Link to="/about">소개</Link>
+        <Link to="/">홈</Link> | <Link to="/about">소개</Link>|{' '}
+        <Link to="/contact">연락처-Ex1</Link>
       </nav>
       <hr />
       {/* 라우트 설정 */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Ex1 />} />
+        <Route path="/profiles/:username" element={<Profile />} />
       </Routes>
     </div>
   );
--- hello-react-vite2/src/ch13/pages/Home.jsx
+++ hello-react-vite2/src/ch13/pages/Home.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
     <div>
       <h1>홈 페이지 🏠</h1>
       <p>환영합니다! 여기는 메인 홈 화면입니다.</p>
+      <li>
+        <Link to="/profiles/lsy">이상용 프로필</Link>
+      </li>
+      <li>
+        <Link to="/profiles/hong">홍길동 프로필</Link>
+      </li>
     </div>
   );
 };
