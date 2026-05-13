9d6c7714f76e813760e293f4202475e0ffa99b05
lsy3709 <lsy3709@naver.com>
2026-05-13 16:14:14 +0900

    7교시, 실습2 확인

--- hello-react-vite2/src/ch15/ex/Ex2.jsx
+++ hello-react-vite2/src/ch15/ex/Ex2.jsx
@@ -2,7 +2,15 @@ import { ColorConsumer } from '../contexts/ColorContext2';
 
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 
-const SelectColors = () => (
+// ### 📝 실습 문제 2
+// > SelectColors 컴포넌트에서 현재 선택된 메인 색상과
+// 서브 색상 이름을 색상 팔레트 아래에 텍스트로 표시하세요.
+
+// > 예: `"메인: blue | 서브: red"`
+
+// > 힌트: `state`도 함께 구조분해 해야 합니다.
+// >
+const Ex2 = () => (
   <div style={{ padding: '10px' }}>
     <h2>색상을 선택하세요.</h2>
     <p style={{ color: '#888', fontSize: '0.85rem' }}>
@@ -31,10 +39,8 @@ const SelectColors = () => (
             ))}
           </div>
           <p>
-            메인 색깔: <span style={{ color: state.color }}>{state.color}</span>{' '}
-            {'|'}
-            보조 색깔:{' '}
-            <span style={{ color: state.subcolor }}>{state.subcolor}</span>
+            메인 색깔: <span>{state.color}</span>
+            보조 색깔: <span>{state.subcolor}</span>
           </p>
         </div>
       )}
@@ -43,4 +49,4 @@ const SelectColors = () => (
   </div>
 );
 
-export default SelectColors;
+export default Ex2;
